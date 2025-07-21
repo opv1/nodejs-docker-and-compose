f64e2a61481f41c65a21a6acff23a324270cfd38
opv1 <pas7273@gmail.com>
2025-07-21 12:07:41 +0300

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3001);
 }
 
